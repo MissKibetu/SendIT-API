e410e3a835a611d3438c8148e28e4f8a1a0b52a5
MissKibetu <mwkibetu@gmail.com>
2018-11-11 11:26:58 +0300

    [chore #161839122] add tests for order creation

--- api/test/test_create_orders.py
+++ api/test/test_create_orders.py
@@ -18,7 +18,22 @@ class TestOrders(unittest.TestCase):
 			"cost" : 200,
 			"status" : "Order requested"
 		}
+
+		self.new_order2 = {
+			"sender_name" : "",
+			"sender_email" : "janedoe@test.com",
+			"pick_up" : "Nairobi",
+			"drop_off" : "Mombasa",
+			"recipient_name" : "John Doe",
+			"weight" : 4,
+			"cost" : 200,
+			"status" : "Order requested"
+		}
 	"""Test order creation"""
 	def test_create_order(self):
 		result = self.client.post('/api/v1/create_order', data = json.dumps(self.new_order1), content_type='application/json')
+		self.assertEqual(result.status_code, 201)
+
+	def test_create_order2(self):
+		result = self.client.post('/api/v1/create_order', data = json.dumps(self.new_order2), content_type='application/json')
 		self.assertEqual(result.status_code, 201)
