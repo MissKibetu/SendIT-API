21579f6ede2794eab62b4778a65ecc709b96b55c
MissKibetu <mwkibetu@gmail.com>
2018-11-20 12:14:08 +0300

    [finish #162080617] Add db_config file to create database tables

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 app = Flask(__name__)
 
+import api.db_config
 import api.v1.orders.views   
      
---
+++ api/db_config.py
@@ -0,0 +1,45 @@
+import os
+import psycopg2
+
+DB_HOST ='localhost'
+DB_USERNAME = 'postgres'
+DB_NAME = 'Send-IT'
+DB_PASS = '1234'
+DB_PORT = '5432'
+
+url = "dbname='Send-IT' host='localhost'\
+                port='5432' user='postgres' password='1234'"
+
+db_url = os.getenv('DATABSE_URL')
+
+#create db connection                
+con = psycopg2.connect(url)
+
+#create the cursor
+cur = con.cursor()
+
+cur.execute(""" CREATE TABLE IF NOT EXISTS users(
+    userID serial PRIMARY KEY,
+    name varchar(50) NOT NULL,
+    email varchar(40) NOT NULL,
+    password varchar(80) NOT NULL,
+    role varchar(10) NOT NULL
+)
+""")
+
+cur.execute(""" CREATE TABLE IF NOT EXISTS orders(
+    parcelID serial PRIMARY KEY,
+    sender_name varchar(50) NOT NULL,
+    sender_email varchar(40) NOT NULL,
+    pick_up varchar(150) NOT NULL,
+    drop_off varchar(150) NOT NULL,
+    recipient_name varchar(50) NOT NULL,
+    weight int NOT NULL,
+    cost int NOT NULL,
+    status varchar(40) NOT NULL,
+    current_location varchar(150) NOT NULL
+)
+""")
+
+con.commit()
+con.close()
