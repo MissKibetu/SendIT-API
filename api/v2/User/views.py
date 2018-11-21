46cffcaeae0d219d1dd464e544363f06e68eb643
MissKibetu <mwkibetu@gmail.com>
2018-11-21 12:32:31 +0300

    [feature #162080580] Add status code to views.py

--- api/v2/User/views.py
+++ api/v2/User/views.py
@@ -7,15 +7,16 @@ usersObject = User()
 @app.route('/api/v2/signup', methods=['POST'])
 def register_user():
     """ User signup """
-    return usersObject.user_signup()
+    return jsonify({"message": usersObject.user_signup()}), 200
+    
 
 @app.route('/api/v2/signin', methods=['POST'])
 def login():
     """ User signin """
-    return usersObject.user_login()
+    return jsonify({"message": usersObject.user_login()}), 200
 
 @app.route('/api/v2/change_role_to_admin/<string:email>', methods=['PUT'])
 def change_role(email):
     """ Change user role to Admin """ 
-    return usersObject.admin_account(email)
+    return jsonify({"message": usersObject.admin_account(email)}), 200
 
